5fb26417ccec1ec8dfaec80db41986512a3359c7
rgayatri <rgayatri@lbl.gov>
2021-08-24 15:43:32 -0700

    Copied the reproducer in default/TestDefaultDeviceDevelop.cpp.

--- core/unit_test/default/TestDefaultDeviceDevelop.cpp
+++ core/unit_test/default/TestDefaultDeviceDevelop.cpp
@@ -51,6 +51,59 @@
 
 namespace Test {
 
-TEST(defaultdevicetype, development_test) {}
+template <typename T, class DeviceType>
+class TestViewAPI {
+ public:
+  using device = DeviceType;
+
+  enum { N0 = 1000, N1 = 3, N2 = 5, N3 = 7 };
+
+  using dView0       = Kokkos::View<T, device>;
+  using dView1       = Kokkos::View<T *, device>;
+  using dView2       = Kokkos::View<T * [N1], device>;
+  using dView3       = Kokkos::View<T * [N1][N2], device>;
+  using dView4       = Kokkos::View<T * [N1][N2][N3], device>;
+  using const_dView4 = Kokkos::View<const T * [N1][N2][N3], device>;
+  using dView4_unmanaged =
+      Kokkos::View<T ****, device, Kokkos::MemoryUnmanaged>;
+  using host = typename dView0::host_mirror_space;
+
+  using DataType = T[2];
+
+
+  static void run_test_error() {
+    // Issue with giving a number to `alloc_size` using numeric_limits for size_t type.
+    // The first one works. The second one fails the test and the third one seg faults. 
+    auto alloc_size = std::numeric_limits<size_t>::max()/2 + 1;
+//    auto alloc_size = std::numeric_limits<size_t>::max()/2 - 1;
+//    auto alloc_size = std::numeric_limits<size_t>::max()/2 - 42;
+
+    try {
+      auto should_always_fail = dView1("hello_world_failure", alloc_size);
+    } catch (std::runtime_error const &error) {
+      // TODO once we remove the conversion to std::runtime_error, catch the
+      //      appropriate Kokkos error here
+      std::string msg = error.what();
+      ASSERT_PRED_FORMAT2(::testing::IsSubstring, "hello_world_failure", msg);
+      ASSERT_PRED_FORMAT2(::testing::IsSubstring,
+                          typename device::memory_space{}.name(), msg);
+      // Can't figure out how to make assertions either/or, so we'll just use
+      // an if statement here for now.  Test failure message will be a bit
+      // misleading, but developers should figure out what's going on pretty
+      // quickly.
+      if (msg.find("is not a valid size") != std::string::npos) {
+        ASSERT_PRED_FORMAT2(::testing::IsSubstring, "is not a valid size", msg);
+      } else
+      {
+        ASSERT_PRED_FORMAT2(::testing::IsSubstring, "insufficient memory", msg);
+      }
+      // SYCL cannot tell the reason why a memory allocation failed
+    }
+  }
+};
+
+TEST(TEST_CATEGORY, view_allocation_error) {
+  TestViewAPI<double, TEST_EXECSPACE>::run_test_error();
+}
 
 }  // namespace Test
