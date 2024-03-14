b3d9918f5c1980182ad75ede900b78c8f4ee2dce
Rahulkumar Gayatri <rgayatri@lbl.gov>
2024-03-13 17:13:02 -0700

    OpenMPTarget: replace desul::atomic_add with omp atomic.

--- core/src/Kokkos_Atomics_Desul_Wrapper.hpp
+++ core/src/Kokkos_Atomics_Desul_Wrapper.hpp
@@ -171,7 +171,15 @@ T atomic_dec_fetch(T* const dest) { return desul::atomic_dec_fetch(dest, desul::
 
 // atomic_op
 template<class T> KOKKOS_INLINE_FUNCTION
-void atomic_add(T* const dest, desul::Impl::dont_deduce_this_parameter_t<const T> val) { return desul::atomic_add (dest, val, desul::MemoryOrderRelaxed(), KOKKOS_DESUL_MEM_SCOPE); }
+void atomic_add(T* const dest, desul::Impl::dont_deduce_this_parameter_t<const T> val) { 
+    // FIXME_OPENMPTARGET - Calling desul::atomic_add is causing illegal memory access issues with the clang compiler.
+#ifdef KOKKOS_ENABLE_OPENMPTARGET
+#pragma omp atomic
+    *dest += val;
+#else
+    return desul::atomic_add (dest, val, desul::MemoryOrderRelaxed(), KOKKOS_DESUL_MEM_SCOPE); 
+#endif
+}
 
 template<class T> KOKKOS_INLINE_FUNCTION
 void atomic_sub(T* const dest, desul::Impl::dont_deduce_this_parameter_t<const T> val) { return desul::atomic_sub (dest, val, desul::MemoryOrderRelaxed(), KOKKOS_DESUL_MEM_SCOPE); }
