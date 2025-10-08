b14b688a1d9cf72a3fc615361455843eaf89a4a4
Rohit-Kakodkar <40695390+Rohit-Kakodkar@users.noreply.github.com>
2025-10-08 12:23:06 -0400

    Adds support for loop unrolling within range loops (#8164)
    Adds a new policy trait StaticBatchSize to control unroll loop factor.
    So far this only affects CUDA RangePolicy based kernels, more to come.
    
    Signed-off-by: Rohit Kakodkar <kakodkar.rohit@gmail.com>
    Co-authored-by: Rohit Kakodkar <kakodkar.rohit@gmail.com>
    Co-authored-by: Bruno Turcksin <bruno.turcksin@gmail.com>

---
+++ core/src/traits/Kokkos_StaticBatchSizeTrait.hpp
@@ -0,0 +1,85 @@
+//@HEADER
+// ************************************************************************
+//
+//                        Kokkos v. 4.0
+//       Copyright (2022) National Technology & Engineering
+//               Solutions of Sandia, LLC (NTESS).
+//
+// Under the terms of Contract DE-NA0003525 with NTESS,
+// the U.S. Government retains certain rights in this software.
+//
+// Part of Kokkos, under the Apache License v2.0 with LLVM Exceptions.
+// See https://kokkos.org/LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//@HEADER
+
+#ifndef KOKKOS_STATIC_BATCH_SIZE_TRAIT_HPP
+#define KOKKOS_STATIC_BATCH_SIZE_TRAIT_HPP
+
+#include <Kokkos_Macros.hpp>
+#include <traits/Kokkos_PolicyTraitAdaptor.hpp>
+#include <traits/Kokkos_Traits_fwd.hpp>
+
+namespace Kokkos::Experimental {
+
+template <unsigned int BatchSize = 1>
+struct StaticBatchSize {
+  using static_batch_size = StaticBatchSize;
+  using type              = StaticBatchSize<BatchSize>;
+  constexpr static unsigned int batch_size{BatchSize};
+
+  static_assert(
+      BatchSize > 0,
+      "Kokkos Error: StaticBatchSize factor must be greater than zero");
+};
+
+}  // end namespace Kokkos::Experimental
+
+namespace Kokkos::Impl {
+
+//==============================================================================
+// <editor-fold desc="trait specification"> {{{1
+
+struct StaticBatchSizeTrait : TraitSpecificationBase<StaticBatchSizeTrait> {
+  struct base_traits {
+    static constexpr bool batch_size_is_defaulted = true;
+
+    using static_batch_size = Kokkos::Experimental::StaticBatchSize<>;
+    KOKKOS_IMPL_MSVC_NVCC_EBO_WORKAROUND
+  };
+  template <class StaticBatchSizeParam, class AnalyzeNextTrait>
+  struct mixin_matching_trait : AnalyzeNextTrait {
+    using base_t = AnalyzeNextTrait;
+    using base_t::base_t;
+
+    static constexpr bool batch_size_is_defaulted = false;
+
+    static_assert(
+        base_t::batch_size_is_defaulted,
+        "Kokkos Error: More than one StaticBatchSizeTrait specified is given.");
+
+    using static_batch_size = StaticBatchSizeParam;
+  };
+};
+}  // end namespace Kokkos::Impl
+
+// </editor-fold> end trait specification }}}1
+//==============================================================================
+
+//==============================================================================
+// <editor-fold desc="PolicyTraitMatcher specialization"> {{{1
+
+namespace Kokkos::Impl {
+
+template <unsigned int BatchSize>
+struct PolicyTraitMatcher<StaticBatchSizeTrait,
+                          Kokkos::Experimental::StaticBatchSize<BatchSize>>
+    : std::true_type {};
+
+// </editor-fold> end PolicyTraitMatcher specialization }}}1
+//==============================================================================
+
+}  // end namespace Kokkos::Impl
+
+#endif  // KOKKOS_STATIC_BATCH_SIZE_TRAIT_HPP
